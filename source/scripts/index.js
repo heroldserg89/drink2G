d96da798dbc14cb89138c15a3a9c8d52964971eb
herold89 <heroldserg89@gmail.com>
2025-08-20 12:28:28 +0300

    Добавляет header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const burger = document.querySelector('.burger');
+const headerNavBlock = document.querySelector('.header__nav-block');
+
+burger.addEventListener('click', () => {
+  headerNavBlock.classList.toggle('header__nav-block--open');
+  burger.classList.toggle('burger--active');
+});
